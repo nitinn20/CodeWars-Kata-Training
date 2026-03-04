979b11d80824e5568e8ea8f1c295e9315646d9fb
NitinN <nitin.nagaral2@gmail.com>
2026-03-04 12:21:54 +0530

    Add solution - CodeHub

---
+++ 7-kyu/categorize-new-member/categorize-new-member.py
@@ -0,0 +1,8 @@
+def open_or_senior(data):
+    result=[]
+    for i in data:
+        if i[0]>=55 and i[1]>7:
+            result.append("Senior")
+        else:    
+            result.append("Open")
+    return result    
