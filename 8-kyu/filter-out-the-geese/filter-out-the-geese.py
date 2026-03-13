c5da7f92588461410f1462eec209dd77ca2a5e3f
NitinN <nitin.nagaral2@gmail.com>
2026-03-13 12:02:54 +0530

    Add solution - CodeHub

---
+++ 8-kyu/filter-out-the-geese/filter-out-the-geese.py
@@ -0,0 +1,7 @@
+geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+def goose_filter(birds):
+    res=[]
+    for i in birds:
+        if i not in geese:
+            res.append(i)
+    return res        
