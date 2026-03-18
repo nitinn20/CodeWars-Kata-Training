97734688da7cd199d7aafaeb2196743919c39e55
NitinN <nitin.nagaral2@gmail.com>
2026-03-18 11:55:27 +0530

    Add solution - CodeHub

---
+++ 8-kyu/switch-it-up/switch-it-up.py
@@ -0,0 +1,16 @@
+def switch_it_up(number):
+    dict={
+        0:'Zero',
+        1:'One',
+        2:'Two',
+        3:'Three',
+        4:'Four',
+        5:'Five',
+        6:'Six',
+        7:'Seven',
+        8:'Eight',
+        9:'Nine'
+    }
+    for k in dict:
+        if k==number:
+            return dict[k]
